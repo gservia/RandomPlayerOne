a772d316e83419bd10b85157961e5b032d93c63c
Gabriel Servia <gaby_servia@hotmail.com>
2021-04-25 23:30:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/randomplayerone/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.randomplayerone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button visit_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.visit_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent viewIntent =
+                        new Intent("android.intent.action.VIEW",
+                                Uri.parse("https://www.youtube.com/channel/UCPmsYa4xMWds-QopCtj94nQ"));
+                startActivity(viewIntent);
+            }
+        });
+
+    }
+
+
+
+
+}
